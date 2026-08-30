8b23a94e67ae4474246c5d632c86fda1573aab0a
Deep Shekhar Singh <deepshekhar0306@gmail.com>
2026-08-30 11:48:33 +0530

    update: product updated by adding a string type for Image URL

--- merchant-server/internal/models/product.go
+++ merchant-server/internal/models/product.go
@@ -11,5 +11,6 @@ type Product struct {
 	PriceINR    float64   `json:"price_inr"`
 	Stock       int       `json:"stock"`
 	Tags        []string  `json:"tags"`
+	ImageURL    string    `json:"image_url"`
 	CreatedAt   time.Time `json:"created_at"`
 }
